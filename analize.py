66a5fc2bef6855102f43a0b833d311c59f694757
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2018-02-19 17:28:47 +0100

    Init commit

---
+++ analize.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+import json
+import ipdb
+
+payload = {'pair' : 'BTCEUR'}
+r = requests.get("https://api.kraken.com/0/public/OHLC", params=payload)
+data = json.loads(r.text)
+A = np.array(data['result']['XXBTZEUR'])
+time = A[:,0]
+open_value = A[:,1]
+ipdb.set_trace()
+#plt.plot(time, open_value) 
+#plt.show() 
