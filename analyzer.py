e92b1ab0784aeecea6effd3d2e06867057c529a6
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2018-02-20 05:12:25 +0100

    switch OHLC infos to trades infos

--- analyzer.py
+++ analyzer.py
@@ -7,19 +7,22 @@ import json
 import ipdb
 
 #pair: altname of pair as given in crypto infos
-#Returns vectors of operations/time
-def getLastOperationsFromOHLC(pair):
+#Returns vectors of time/operations
+def getLastOperationsFromTradesInfos(pair):
 	payload = {'pair' : pair}
-	r = requests.get("https://api.kraken.com/0/public/OHLC", params=payload)
+	r = requests.get("https://api.kraken.com/0/public/Trades", params=payload)
 	data = json.loads(r.text)
 	A = np.array(data['result'][pair])
 	#ipdb.set_trace()
-	return A[:,0:2]
+	#A = <price>, <volume>, <time>, <buy/sell>, <market/limit>, <miscellaneous>
+	return np.column_stack((A[:,2],A[:,0]))
 	#time = A[:,0]
 	#open_value = A[:,1]
 	#ipdb.set_trace()
 
-operations = getLastOperationsFromOHLC('XXBTZEUR')
+operations = getLastOperationsFromTradesInfos('XXBTZEUR')
 
-plt.plot(operations[:,0], operations[:,1]) 
+
+ipdb.set_trace()
+plt.plot(operations[:,1]) #Plots ops from the last 1000 minutes
 plt.show() 
