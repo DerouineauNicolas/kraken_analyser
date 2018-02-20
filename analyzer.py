132f0515505a63cff936f2543a88c70593b8321c
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2018-02-20 04:59:31 +0100

    Use altname for pair input

--- analyzer.py
+++ analyzer.py
@@ -6,19 +6,20 @@ import requests
 import json
 import ipdb
 
+#pair: altname of pair as given in crypto infos
 #Returns vectors of operations/time
-def getLastOperations(pair):
+def getLastOperationsFromOHLC(pair):
 	payload = {'pair' : pair}
 	r = requests.get("https://api.kraken.com/0/public/OHLC", params=payload)
 	data = json.loads(r.text)
-	A = np.array(data['result']['XXBTZEUR'])
+	A = np.array(data['result'][pair])
 	#ipdb.set_trace()
 	return A[:,0:2]
 	#time = A[:,0]
 	#open_value = A[:,1]
 	#ipdb.set_trace()
 
-operations = getLastOperations('BTCEUR')
+operations = getLastOperationsFromOHLC('XXBTZEUR')
 
 plt.plot(operations[:,0], operations[:,1]) 
 plt.show() 
