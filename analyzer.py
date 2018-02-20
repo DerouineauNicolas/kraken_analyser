23db94403304958316c44594c6ee7e8912bc28f4
Nicolas DEROUINEAU <nicolas.derouineau@telecom-paristech.fr>
2018-02-20 04:29:17 +0100

    Create function to get last operations

--- analyzer.py
+++ analyzer.py
@@ -6,12 +6,19 @@ import requests
 import json
 import ipdb
 
-payload = {'pair' : 'BTCEUR'}
-r = requests.get("https://api.kraken.com/0/public/OHLC", params=payload)
-data = json.loads(r.text)
-A = np.array(data['result']['XXBTZEUR'])
-time = A[:,0]
-open_value = A[:,1]
-#ipdb.set_trace()
-plt.plot(time, open_value) 
+#Returns vectors of operations/time
+def getLastOperations(pair):
+	payload = {'pair' : pair}
+	r = requests.get("https://api.kraken.com/0/public/OHLC", params=payload)
+	data = json.loads(r.text)
+	A = np.array(data['result']['XXBTZEUR'])
+	#ipdb.set_trace()
+	return A[:,0:2]
+	#time = A[:,0]
+	#open_value = A[:,1]
+	#ipdb.set_trace()
+
+operations = getLastOperations('BTCEUR')
+
+plt.plot(operations[:,0], operations[:,1]) 
 plt.show() 
